e64db76256d34b092303f755bd7c26563c75846d
maoyanluo <mengxin_zzl@outlook.com>
2025-01-05 03:43:31 +0800

    项目创建

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # anywhere-door-plugin-pve
 AnywhereDoor Plugin PVE实现
+
+## 环境变量
+* HOST: 控制平面地址
+* PORT: 控制平面端口
+* PREFIX: 控制平面前缀
+* USERNAME: plugin所属用户名称
+* TOKEN: plugin令牌
+* PLUGIN_NAME: plugin名称
+
+## 打包方式
+1. 将代码clone下来
+2. 安装docker及buildx
+3. 打包镜像
+    * `docker buildx build --platform linux/amd64 -t 192.168.25.5:31100/maoyanluo/anywhere-door-plugin-pve:1.0 . --load`
+
+## 部署方式
+
+### Docker Command Line
+1. 运行容器
+
+### Kubernetes
+```yaml
+
+```
+
+1. 保证容器正常运行
+2. 注册plugin: POST AnywhereDoorManager/plugin/create & Header: token: token & Body: { "plugin_name": "name", "plugin_describe": "desc", "plugin_host": "anywhere-door-plugin-pve-service.anywhere-door", "plugin_port": 80, "plugin_token": "token" }
+3. 增加plugin配置信息: POST AnywhereDoorManager/config/create & Header: token: token & Body: [{"name": "pve name", "address": "https://pve.host:8006", "user": "admin user", "token": "token"}]
+
---
+++ src/index.js
@@ -0,0 +1,136 @@
+import axios from 'axios'
+import Koa from 'koa'
+import Router from 'koa-router'
+import constant from './constant.js'
+import cmdConstant from './cmd_constant.js'
+import fs from 'fs'
+
+async function processCommand(axiosControlPlane, raw, extend) {
+    const cmds = raw.split(' ')
+    if (cmds.length >= 1) {
+        const method = cmds[0]
+        if (method === 'help') {
+            return await new Promise((resolve, reject) => {
+                fs.readFile('src/help', 'utf8', (err, data) => {
+                    if (err) {
+                        reject(err)
+                    } else {
+                        resolve(data)
+                    }
+                })
+            })
+        }
+        // Todo
+
+        switch (method) {
+
+            default:
+                return cmdConstant.RESULT_NO_SUCH_CMD
+        }
+    } else {
+        return cmdConstant.RESULT_PARAMS_ERROR
+    }
+}
+
+async function sendRequest(axiosControlPlane, pluginName, target, data) {
+    return await axiosControlPlane.post(constant.PLUGIN_URL, {
+        name: pluginName,
+        target: target,
+        data: data
+    })
+}
+
+function main() {
+    const host = process.env.HOST
+    const port = process.env.PORT
+    const prefix = process.env.PREFIX || ''
+    const username = process.env.USERNAME
+    const token = process.env.TOKEN
+    const pluginName = process.env.PLUGIN_NAME
+
+    const axiosControlPlane = axios.create({
+        baseURL: constant.CONTROL_PLANE_BASE_REQUEST_URL(host, port, prefix),
+        headers: {
+            token, username
+        }
+    })
+
+    const app = new Koa()
+    const router = new Router()
+    router.post(constant.PLUGIN_URL, ctx => {
+        let _token = ctx.request.headers.token
+        ctx.req.on(constant.EVENT_DATA, data => {
+            let params = JSON.parse(data)
+            let name = params[constant.PARAMS_NAME]
+            let target = params[constant.PARAMS_TARGET]
+            let raw = params[constant.PARAMS_DATA]
+            if (target !== pluginName) {
+                return ctx.response.body = JSON.stringify({
+                    code: constant.ERROR_CODE_NOT_THIS_PLUGIN,
+                    message: constant.ERROR_MESSAGE_NOT_THS_PLUGIN
+                })
+            }
+            if (_token !== token) {
+                return ctx.response.body = JSON.stringify({
+                    code: constant.ERROR_CODE_TOKEN_INVALID,
+                    message: constant.ERROR_MESSAGE_TOKEN_INVALID
+                })
+            }
+            const extend = {}
+            extend[constant.PLUGIN_NAME] = pluginName
+            processCommand(axiosControlPlane, raw, extend).then(data => {
+                sendRequest(axiosControlPlane, pluginName, name, data).then(res => {
+                    console.log(res)
+                }).catch(err => {
+                    console.log(err)
+                })
+            }).catch(err => {
+                console.log(err)
+                sendRequest(axiosControlPlane, pluginName, name, err.message).then(res => {
+                    console.log(res)
+                }).catch(err => {
+                    console.log(err)
+                })
+            })
+            ctx.response.body = JSON.stringify({
+                code: constant.ERROR_CODE_SUCCESS,
+                message: constant.ERROR_MESSAGE_SUCCESS
+            })
+        })
+    })
+    app.use(router.routes()).use(router.allowedMethods())
+    app.listen(80)
+}
+
+// main()
+
+(async () => {
+    // const axiosControlPlane= axios.create({
+    //     baseURL: `http://${host}:${port}${prefix}`,
+    //     headers: {
+    //         token, username
+    //     }
+    // })
+    // const config = JSON.parse((await axiosControlPlane.post('/plugin/config', {
+    //     name: pluginName,
+    //     config_key: 'hosts'
+    // })).data.data.config_value)
+    // console.log(config)
+
+    const promise = new Promise((resolve, reject) => {
+        fs.readFile('src/help', 'utf8', (err, data) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(data)
+            }
+        })
+    })
+
+    // promise.then(res => {
+    //     console.log(res)
+    // })
+
+
+    console.log(await promise)
+})().then()
